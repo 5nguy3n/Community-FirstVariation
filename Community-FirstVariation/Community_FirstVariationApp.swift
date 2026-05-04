a4b6b696b9f577c6f68ea3f98a5cc8acad473dae
5nguy3n <sarahnguyen4321@gmail.com>
2026-05-04 16:22:50 +1000

    Initial Commit

---
+++ Community-FirstVariation/Community_FirstVariationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Community_FirstVariationApp.swift
+//  Community-FirstVariation
+//
+//  Created by Sarah Nguyen on 4/5/2026.
+//
+
+import SwiftUI
+
+@main
+struct Community_FirstVariationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
